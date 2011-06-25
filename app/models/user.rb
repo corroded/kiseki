5247d1240de352e9a0af852c66d424f53d264651
Eumir Gaspar <eumir@aelogica.com>
2011-06-25 22:39:45 +0800

    added login field to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  validates_presence_of :login
+  validates_uniqueness_of :login, :email, :case_sensitive => false
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :login, :email, :password, :password_confirmation, :remember_me
 end
