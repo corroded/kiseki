f94253e12df52caecf193aa587d028ebf031d694
Eumir Gaspar <eumir@aelogica.com>
2011-06-26 01:20:24 +0800

    took out name from cucumber features. all tests passing

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -2,9 +2,8 @@ Given /^no user exists with an email of "(.*)"$/ do |email|
   User.find(:first, :conditions => { :email => email }).should be_nil
 end
 
-Given /^I am a user named "([^"]*)" with an email "([^"]*)" and password "([^"]*)"$/ do |name, email, password|
-  User.new(:name => name,
-            :email => email,
+Given /^I am a user with an email "([^"]*)" and password "([^"]*)"$/ do |email, password|
+  User.new(:email => email,
             :password => password,
             :password_confirmation => password).save!
 end
