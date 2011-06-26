6e7498d2e758a4b304ee152399cfc1734562b019
Eumir Gaspar <eumir@aelogica.com>
2011-06-27 01:05:10 +0800

    bootstrapped spork to rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,40 +1,24 @@
 require 'rubygems'
 require 'spork'
 
+ENV["RAILS_ENV"] ||= 'test'
+
 Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However, 
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
+
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
   
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
 end
 
 Spork.each_run do
   # This code will be run each time you run your specs.
   
 end
-
-# --- Instructions ---
-# - Sort through your spec_helper file. Place as much environment loading 
-#   code that you don't normally modify during development in the 
-#   Spork.prefork block.
-# - Place the rest under Spork.each_run block
-# - Any code that is left outside of the blocks will be ran during preforking
-#   and during each_run!
-# - These instructions should self-destruct in 10 seconds.  If they don't,
-#   feel free to delete them.
-#
-
-
-
-
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
-
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   # == Mock Framework
